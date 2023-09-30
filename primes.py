738c46ed90405466e0dba00cc2f42c3daaf222d9
Vyom Fadia <vyom.fadia.work@gmail.com>
2023-09-30 11:59:22 +0100

    feat: implement naive prime calculation method

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = []
-    return list
+import typing
+
+def primes(number_of_primes: int) -> typing.List[int]:
+    primes = []
+
+    i = 2
+    while len(primes) < number_of_primes:
+        if all([i % x != 0 for x in primes]):
+            primes.append(i)
+
+        i += 1
+
+    return primes
